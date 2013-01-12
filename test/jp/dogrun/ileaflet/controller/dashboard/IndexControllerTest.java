c8ac82914b78e8864bb47c761e87bb6a29402b1b
Yuzo Hebishima <secondarykey@gmail.com>
2013-01-12 19:22:21 +0900

    作業ページの作成

---
+++ test/jp/dogrun/ileaflet/controller/dashboard/IndexControllerTest.java
@@ -0,0 +1,18 @@
+package jp.dogrun.ileaflet.controller.dashboard;
+
+import org.slim3.tester.ControllerTestCase;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class IndexControllerTest extends ControllerTestCase {
+
+    @Test
+    public void run() throws Exception {
+        tester.start("/dashboard/");
+        IndexController controller = tester.getController();
+        assertThat(controller, is(notNullValue()));
+        assertThat(tester.isRedirect(), is(false));
+        assertThat(tester.getDestinationPath(), is("/dashboard/index.jsp"));
+    }
+}
