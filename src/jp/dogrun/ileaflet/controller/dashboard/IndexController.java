8ec0c6e68e11ba50c201cece6f7decee38c288b9
Yuzo Hebishima <secondarykey@gmail.com>
2013-01-12 19:42:59 +0900

    ログイン状況による遷移

--- src/jp/dogrun/ileaflet/controller/dashboard/IndexController.java
+++ src/jp/dogrun/ileaflet/controller/dashboard/IndexController.java
@@ -1,5 +1,7 @@
 package jp.dogrun.ileaflet.controller.dashboard;
 
+import jp.dogrun.ileaflet.model.Actor;
+
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
 
@@ -7,6 +9,12 @@ public class IndexController extends Controller {
 
     @Override
     public Navigation run() throws Exception {
+        
+        Actor actor = (Actor)request.getSession().getAttribute(Actor.class.getName());
+        //ログイン状態を取得
+        if ( actor == null ) {
+            return forward("../login/");
+        }
         return forward("index.jsp");
     }
 }
