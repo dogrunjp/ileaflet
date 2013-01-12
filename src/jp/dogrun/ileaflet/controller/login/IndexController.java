d997bdf8297a0299d4be7f887aefb79a4757daab
Yuzo Hebishima <secondarykey@gmail.com>
2013-01-12 19:26:13 +0900

    ログイン状態による遷移を追加

--- src/jp/dogrun/ileaflet/controller/login/IndexController.java
+++ src/jp/dogrun/ileaflet/controller/login/IndexController.java
@@ -1,5 +1,7 @@
 package jp.dogrun.ileaflet.controller.login;
 
+import jp.dogrun.ileaflet.model.Actor;
+
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
 
@@ -7,6 +9,13 @@ public class IndexController extends Controller {
 
     @Override
     public Navigation run() throws Exception {
+
+        Actor actor = (Actor)request.getSession().getAttribute(Actor.class.getName());
+        //ログイン状態を取得
+        if ( actor != null ) {
+            return forward("../dashboard/");
+        }
+        //ログインページに移動
         return forward("index.jsp");
     }
 }
